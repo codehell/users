53057d669bf9e6ec195c48e5e215d6e084ea22a2
dmorales <dmorales@inespay.com>
2020-01-01 21:07:07 +0100

    add json tags to user struct

--- users.go
+++ users.go
@@ -1,15 +1,88 @@
 package users
 
 import (
+	"errors"
+	"github.com/alexedwards/argon2id"
 	"time"
 )
 
 type User struct {
-	ID        interface{}
-	Username  string
-	Email     string
-	Password  string
-	Role      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        interface{} `json:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Role      string `json:"role"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type Manager struct {
+	client    Client
+	validator Validator
+}
+
+type Client interface {
+	Create(*User) error
+	Close() error
+	GetUserByEmail(string) (User, error)
+	GetAll() ([]User, error)
+	DeleteAll() error
+}
+
+type Validator func(user User) error
+
+func NewManager(c Client) *Manager {
+	um := new(Manager)
+	um.client = c
+	um.validator = defaultValidator
+	return um
+}
+
+func (um *Manager) CreateUser(u *User) error {
+	if err := defaultValidator(*u); err != nil {
+		return err
+	}
+	password, err := generatePassword(u.Password)
+	if err != nil {
+		return err
+	}
+	u.Password = password
+	u.CreatedAt = time.Now()
+	u.UpdatedAt = time.Now()
+	return um.client.Create(u)
+}
+
+func (um *Manager) GetUserByEmail(email string) (User, error) {
+	return um.client.GetUserByEmail(email)
+}
+
+func (um *Manager) GetUsers() ([]User, error) {
+	return um.client.GetAll()
+}
+
+func (um *Manager) Close() error {
+	if um.client != nil {
+		return um.client.Close()
+	}
+	return errors.New("client is nil")
+}
+
+func (um *Manager) SetValidator(val Validator) {
+	um.validator = val
+}
+
+func (um *Manager) CheckPassword(u User, password string) bool {
+	match, err := argon2id.ComparePasswordAndHash(password, u.Password)
+	if err != nil {
+		return false
+	}
+	return match
+}
+
+func generatePassword(password string) (string, error) {
+	hash, err := argon2id.CreateHash(password, argon2id.DefaultParams)
+	if err != nil {
+		return "", err
+	}
+	return hash, nil
 }
--- users_test.go
+++ users_test.go
@@ -4,6 +4,66 @@ import (
 	"testing"
 )
 
+func TestCreateUser(t *testing.T) {
+	user := getTestingUser()
+	user.Role = "admin"
+	client, err := NewClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	manager := NewManager(client)
+	err = manager.CreateUser(&user)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if client != nil {
+			if err := manager.Close(); err != nil {
+				t.Fatal(err)
+			}
+		}
+	}()
+	storedUser, err := manager.GetUserByEmail(user.Email)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if storedUser.Email != user.Email {
+		t.Error("The user was not the expected user")
+	}
+	if err = client.DeleteAll(); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestGetUsers(t *testing.T) {
+	client, err := NewClient()
+	if err != nil {
+		t.Fatal("can not create client")
+	}
+	manager := NewManager(client)
+	defer func() {
+		err = manager.Close()
+		if err != nil {
+			t.Fatal("can not close client")
+		}
+	}()
+	if err := createTwentyUsers(manager); err != nil {
+		t.Error(err)
+	}
+	myUsers, err := manager.GetUsers()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(myUsers) != 20 {
+		t.Errorf("got %d users, expect %d users", len(myUsers), 20)
+	}
+
+	if err = client.DeleteAll(); err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestOkPassword(t *testing.T) {
 	user := getTestingUser()
 	client, err := NewClient()
